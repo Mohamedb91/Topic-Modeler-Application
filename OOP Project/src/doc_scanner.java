e1b906f3e83d467def94b141a7fdf61467b7cce7
Mohamedb91 <mohamed.benyounis@gmail.com>
2024-03-28 16:34:59 +0000

    Doc Scanner
    Made the doc_scanner class process the document that is being inputted i.e. unwanted characters are removed ( special characters and punctuation)
    doc is then split into words and saved in an array i believe.

--- OOP Project/src/doc_scanner.java
+++ OOP Project/src/doc_scanner.java
@@ -1,10 +1,34 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class doc_scanner {
 	
-	String doc_path = "README";
-	File doc = new File("README");	
-	Scanner scan = new Scanner(doc);
+	private File file;
+	private List<String> words;
+	
+	public doc_scanner(String filename) {
+		this.file = new File(filename);
+		this.words = new ArrayList<>();
+		try {
+			Scanner scan = new Scanner(file);
+			while(scan.hasNextLine()) {
+				String line = scan.nextLine();
+				String[] lineToWords = line.split("\\s+");
+				for(String word : lineToWords) {
+					word = word.toLowerCase();
+					word = word.replace("^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$", "");
+					words.add(word);				
+				}
+			}
+			scan.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public List<String> getWords(){
+		return words;
+	}
 	
 }
--- OOP Project/src/main.java
+++ OOP Project/src/main.java
@@ -1,9 +1,17 @@
+import java.util.List;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String filename = "C:/Users/Mohamed/Documents/ahahahaha.txt";
+		doc_scanner doc1 = new doc_scanner(filename);
+		List<String> processedWords = doc1.getWords();
 
+		// Print the processed words
+		for (String word : processedWords) {
+			System.out.println(word);
+		}	
 	}
 
 }
