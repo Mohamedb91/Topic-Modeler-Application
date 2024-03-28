1532553456a354849640ff75ff195ae667015042
Mohamedb91 <mohamed.benyounis@gmail.com>
2024-03-28 00:50:36 +0000

    oop project

---
+++ OOP Project/src/doc_scanner.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class doc_scanner {
+	
+	String doc_path = "README";
+	File doc = new File("README");	
+	Scanner scan = new Scanner(doc);
+	
+}
