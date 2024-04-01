ff013092e0df7a92359b2de26153791ff21574a1
Mohamedb91 <mohamed.benyounis@gmail.com>
2024-04-01 17:46:15 +0100

    Update main.java

--- OOP Project/src/main.java
+++ OOP Project/src/main.java
@@ -4,9 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		String filename = "README.txt"; //add the path to the file you want to pick
-		doc_scanner doc1 = new doc_scanner(filename);
-		List<String> processedWords = doc1.getWords();
+		String filename = "README.txt";
+		doc_scanner file1 = new doc_scanner(filename);
+		List<String> processedWords = file1.getWords();
 
 		// Print the processed words
 		for (String word : processedWords) {
