cdfe9ce9aecc93176100eff77ef4d1829cabc111
CillianDwyer <165103177+CillianDwyer@users.noreply.github.com>
2024-04-05 15:55:51 +0100

    Update main.java

--- OOP Project/src/main.java
+++ OOP Project/src/main.java
@@ -4,6 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		
+		SwingUtilities.invokeLater(() -> {
+            	TopicModelerGUI gui = new TopicModelerGUI();
+            	gui.setVisible(true);
+       	        });
+		
 		String filename = "README.txt";
 		doc_scanner file1 = new doc_scanner(filename);
 		List<String> processedWords = file1.getWords();
@@ -11,8 +16,12 @@ public class main {
 		// Print the processed words
 		for (String word : processedWords) {
 			System.out.println(word);
-		}	
+		}
 		
 	}
 
 }
+
+
+        
+    
